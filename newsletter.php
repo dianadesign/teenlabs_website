99aed110b048f1b0ee075c6f44ff6ede600107ce
DIANA MIRON <diana@ap3.ro>
2017-10-10 16:07:43 +0100

    New structure

--- newsletter.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mail = $_POST['mail'];
-$formcontent=" O noua persoana s-a inscris in comunitatea Teen Labs. Email: $mail.";
-$recipient = "newsletter@teenlabs.ro";
-$subject = "Un nou mail";
-mail($recipient, $subject, $formcontent) or die("Error!");
-header("location:index.html");
-?>
